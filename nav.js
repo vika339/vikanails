a3b53e04cc2f0b8dd8e1278dc281663e4d1260de
Виктория Вознюк < >
2016-09-04 14:05:51 +0300

    версия для локального компа

---
+++ nav.js
@@ -0,0 +1,7 @@
+var menu='<ul>';
+menu+='<li><a href="index.html">Главная</a></li>';
+menu+=' <li><a href="contact.html">Контакты</a></li>';
+menu+='<li><a href="images.html">Галерея</a></li>';
+menu+='<li><a href="price.html">Цены</a></li>';
+menu+='<li><a href="article.html">Статьи</a></li>';
+document.getElementById('nav').innerHTML=menu;
